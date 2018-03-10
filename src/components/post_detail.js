539a2f967647bf18f4d60775ef8588435851d0ca
XU SHIYAN <xu.shiyan.raymond@gmail.com>
2018-03-11 01:30:25 +0800

    Update post comment count when add/delete comment

--- src/components/post_detail.js
+++ src/components/post_detail.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -13,23 +14,23 @@ class PostDetail extends PostPreview {
         this.props.getPost(post_id);
     }
 
-    showPost(post) {
+    showPost(post, numComments) {
         return (
             <div>
                 <button>Edit</button>
                 <button>Delete</button>
                 <h2>{post.title}</h2>
-                <h4>{post.author}</h4>
+                <h4>Posted by {post.author}</h4>
                 <p>{post.body}</p>
                 <Voter businessObject={post} updateVoteScore={this.props.updatePostVoteScore} />
-                <p>{post.voteScore}</p>
-                <p>{post.commentCount}</p>
+                <p>{post.voteScore} votes</p>
+                <p>{numComments} comments</p>
             </div>
         );
     }
 
     render() {
-        const { post } = this.props;
+        const { post, numComments } = this.props;
         if (!post)
             return <div>Loading...</div>
 
@@ -37,15 +38,16 @@ class PostDetail extends PostPreview {
             <div>
                 <Link to='/'>Back to All Posts</Link>
                 <Link to={`/${post.category}`}>Back to Posts in {post.category}</Link>
-                {this.showPost(post)}
+                {this.showPost(post, numComments)}
                 <CommentsList postId={post.id} />
             </div>
         );
     }
 }
 
-const mapStateToProps = ({ posts }, ownProps) => {
+const mapStateToProps = ({ posts, activePostComments }, ownProps) => {
     const post = posts[ownProps.match.params.post_id];
-    return { post };
+    const numComments = _.size(activePostComments);
+    return { post, numComments };
 };
 export default connect(mapStateToProps, { getPost, updatePostVoteScore })(PostDetail);
